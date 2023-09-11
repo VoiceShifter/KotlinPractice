eaaf05d7a9123636aabe2c5f7feba85e94ec672a
unknown <gachi.muchi.98@mail.ru>
2023-09-11 03:22:10 +0300

    git init

---
+++ app/src/main/java/com/example/kotlin_practice1/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.kotlin_practice1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    val FirstFragmentVariable = PasswordFragment()
+    val SecondFragmentVariable = TodoList()
+    val ThirdFragmentVariable = Schedule()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.ContainerFragment, FirstFragmentVariable)
+            commit()
+
+        }
+
+    }
+
+    fun FragmentMovementButton(view: View)
+    {
+        val currentFragment = supportFragmentManager.fragments.last()
+
+        if(currentFragment ==  FirstFragmentVariable)
+
+        {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.ContainerFragment, SecondFragmentVariable)
+                commit()
+
+            }
+        }
+        else if (currentFragment == SecondFragmentVariable) {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.ContainerFragment, ThirdFragmentVariable)
+                commit()
+
+            }
+        }
+        else
+        {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.ContainerFragment, FirstFragmentVariable)
+                commit()
+
+            }
+
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/kotlin_practice1/Schedule.kt
@@ -0,0 +1,19 @@
+package com.example.kotlin_practice1
+
+import androidx.fragment.app.Fragment
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Schedule.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Schedule : Fragment(R.layout.schedule) {
+
+
+
+}
